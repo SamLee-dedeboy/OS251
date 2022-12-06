0cfd0ab0232c2362e72bb7b19537c933f2d7e6bc
SamLee-dedeboy <samytleezzz@gmail.com>
2022-12-05 21:05:26 -0800

    merge kernel

--- kernel/src/startup.c
+++ kernel/src/startup.c
@@ -1,5 +1,9 @@
 #include <stdint.h>
-#include "Systemcall.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "chip.h"
+#include "syscallinterr.h"
+#include "thread.h"
 
 extern uint8_t _erodata[];
 extern uint8_t _data[];
@@ -9,56 +13,33 @@ extern uint8_t _esdata[];
 extern uint8_t _bss[];
 extern uint8_t _ebss[];
 
-// Adapted from https://stackoverflow.com/questions/58947716/how-to-interact-with-risc-v-csrs-by-using-gcc-c-code
-__attribute__((always_inline)) inline uint32_t csr_mstatus_read(void)
-{
-    uint32_t result;
-    asm volatile("csrr %0, mstatus"
-                 : "=r"(result));
-    return result;
-}
-
-__attribute__((always_inline)) inline void csr_mstatus_write(uint32_t val)
-{
-    asm volatile("csrw mstatus, %0"
-                 :
-                 : "r"(val));
-}
-
-__attribute__((always_inline)) inline void csr_write_mie(uint32_t val)
-{
-    asm volatile("csrw mie, %0"
-                 :
-                 : "r"(val));
-}
-
-__attribute__((always_inline)) inline void csr_enable_interrupts(void)
-{
-    asm volatile("csrsi mstatus, 0x8");
-}
-
-__attribute__((always_inline)) inline void csr_disable_interrupts(void)
-{
-    asm volatile("csrci mstatus, 0x8");
-}
-
-#define MTIME_LOW (*((volatile uint32_t *)0x40000008))
-#define MTIME_HIGH (*((volatile uint32_t *)0x4000000C))
-#define MTIMECMP_LOW (*((volatile uint32_t *)0x40000010))
-#define MTIMECMP_HIGH (*((volatile uint32_t *)0x40000014))
-#define CONTROLLER (*((volatile uint32_t *)0x40000018))
-#define MODE_CONTROL_REG (*((volatile uint32_t *)0x500FF414))
-#define MACHINE_TIME_REGISTER (*((volatile uint32_t *)0x40000008))
-#define MACHINE_PERIOD_REGISTER (*((volatile uint32_t *)0x40000044))
+typedef void (*Tfunc)(void);
 
 volatile uint32_t *smallspritecontrol = (volatile uint32_t *)(0x500FF214);
+volatile uint32_t *BGpalette0 = (volatile uint32_t *)0x500FC000;
+volatile uint32_t *IER = (volatile uint32_t *)(0x40000000);
+volatile uint32_t *IPR = (volatile uint32_t *)(0x40000004);
+// Thread variable
+uint32_t ThreadStack[9][2048];
+TStackRef ThreadPointers[10];
+int current_thread_num = 1;
+int running_thread_pointer = 0;
+
+volatile int video = 0;
+
+extern volatile int global;
+extern volatile uint32_t controller_status;
+volatile uint32_t *INT_PENDING_REG = (volatile uint32_t *)(0x40000004);
+int color = 1;
+int color_counter = 0;
+
+static unsigned int seed = 1;
 
 void init(void)
 {
     uint8_t *Source = _erodata;
     uint8_t *Base = _data < _sdata ? _data : _sdata;
     uint8_t *End = _edata > _esdata ? _edata : _esdata;
-
 
     while (Base < End)
     {
@@ -76,68 +57,124 @@ void init(void)
     MTIMECMP_LOW = 1;
     MTIMECMP_HIGH = 0;
 
-    // /*Sprite Control and palette initialized
-    //     ctr_bits = 0001 1111 1110 0001 0000 0000 0100 0000 */
-    // volatile uint32_t *palette0 = (volatile uint32_t *)(0x500FD000);
-    // volatile uint32_t *palette1 = (volatile uint32_t *)(0x500FD400);
-
-
-    // for (int i = 0; i < 256; i++)
-    // {
-    //     palette0[i] = 0xff000000 + i;
-    //     // palette1[i] = 0xff0000ff - i;
-    // }
-
-    // // init small sprite location
-    // // 0x 000 1111 1110 000010000 0000010000 00
-    // smallspritecontrol[0] = 0x1fc10040;
+    // *IER = *IER | COMMAND_BIT;
+    // *IER = *IER | VIDEO_BIT;
+    // *IER = *IER | CART_BIT;
 }
 
-extern volatile int global;
-extern volatile uint32_t controller_status;
-volatile uint32_t *INT_PENDING_REG = (volatile uint32_t *)(0x40000004);
-int color = 1;
-int color_counter = 0;
-
-static unsigned int seed = 1;
-void srand (uint32_t newseed) {
+void c_srand(uint32_t newseed)
+{
     seed = (unsigned)newseed & 0x7fffffffU;
 }
-uint32_t rand (void) {
+
+uint32_t c_rand(void)
+{
     seed = (seed * 1103515245U + 12345U) & 0x7fffffffU;
     return (uint32_t)seed;
 }
 
-void c_interrupt_handler(uint32_t mcause)
+void c_interrupt_handler(void)
+{
+    uint32_t mcause = csr_mcause_read();
+    switch (mcause)
+    {
+    case TIMER_INTERRUPT:
+        timer_interrupt();
+        break;
+    case ILLEGAL_INST_INTERRUPT:
+        illegal_inst_interrupt();
+        break;
+    case EXTERNAL_INTERRUPT:
+    {
+        // if (*IPR & COMMAND_BIT)
+        // {
+        //     cmd_interrupt();
+        //     return;
+        // }
+        if (*IPR & VIDEO_BIT)
+        {
+            video_interrupt();
+            return;
+        }
+        if (*IPR & CART_BIT)
+        {
+            cart_interrupt();
+            return;
+        }
+    }
+    default:
+        break;
+    }
+    (*INT_PENDING_REG) |= ~(1U << 2);
+}
+
+void illegal_inst_interrupt()
+{
+    csr_write_mepc(RESTART_ADDRESS);
+}
+
+void timer_interrupt()
 {
     uint64_t NewCompare = (((uint64_t)MTIMECMP_HIGH) << 32) | MTIMECMP_LOW;
     NewCompare += 100;
     MTIMECMP_HIGH = NewCompare >> 32;
     MTIMECMP_LOW = NewCompare;
     global++;
-    controller_status = CONTROLLER;
-
-    // CMD control
-    // if ((((*INT_PENDING_REG) & 0x4) >> 2))
-    // {
-    //     if (MODE_CONTROL_REG == 0x1)
-    //         MODE_CONTROL_REG = 0x00000000;
-    //     else if (MODE_CONTROL_REG == 0x0) {
-    //         MODE_CONTROL_REG = 0x00000001;
-    //     }
-    // }
-    (*INT_PENDING_REG) |= ~(1U << 2);
-    // CMD control end
+    if (current_thread_num >= 2)
+    {
+        uint32_t mepc = csr_mepc_read();
+        printf("\n");
+        TInterruptState PrevState = CPUHALSuspendInterrupts();
+        int t1, t2;
+        t1 = running_thread_pointer;
+        t2 = (running_thread_pointer + 1) % current_thread_num;
+        running_thread_pointer = t2;
+        ContextSwitch(&ThreadPointers[t1], ThreadPointers[t2]);
+        csr_write_mepc(mepc);
+        CPUHALResumeInterrupts(PrevState);
+    }
+    if (global == 9999)
+    {
+        global = 0;
+    }
 }
 
-typedef uint32_t *TContext;
-typedef void (*TEntry)(void *);
-
-TContext InitContext(uint32_t *stacktop, TEntry entry, void *param);
-
-uint32_t c_system_call(uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, uint32_t call)
+void cmd_interrupt()
 {
-    switch (a0)
+    if (MODE_CONTROL & 1)
+    {
+        MODE_CONTROL = 0x00000000;
+    }
+    else
+    {
+        MODE_CONTROL = 0x00000001;
+    }
+    *IPR = *IPR & COMMAND_BIT;
+}
+void video_interrupt()
+{
+    video++;
+    if (video >= 1000)
+    {
+        video = 0;
+    }
+    *IPR = *IPR & VIDEO_BIT;
+}
+void cart_interrupt()
+{
+    if (CARTIDGE_STATUS & CIS)
+    {
+        *IPR = *IPR & CART_BIT;
+    }
+    else
+    {
+        *IPR = *IPR & CART_BIT;
+    }
+}
+
+uint32_t c_system_call(uint32_t param1, uint32_t param2, uint32_t param3, uint32_t param4)
+{
+    switch (param1)
     {
     case SYSTIMER:
         return global;
@@ -148,12 +185,20 @@ uint32_t c_system_call(uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3, uint3
         break;
 
     case MODE_STATUS:
-        return MODE_CONTROL_REG;
+        if (MODE_CONTROL & 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
         break;
 
     case SMALL_SPRITE_DROP:
         smallspritecontrol[0] += 0x00001000;
         return 1;
+        break;
 
     case READ_MACHINE_TIME:
         return MACHINE_TIME_REGISTER;
@@ -163,17 +208,27 @@ uint32_t c_system_call(uint32_t a0, uint32_t a1, uint32_t a2, uint32_t a3, uint3
 
     case READ_INT_PENDING_REG:
         return INT_PENDING_REG;
-    
-    case RAND:
-        srand(MACHINE_TIME_REGISTER);
-        return rand();
-    }
 
-    // Thread
-    if (call == 3)
-    {
-        uint32_t ThreadStack[128];
-        return InitContext(ThreadStack + 128, (void *)0, (void *)0);
+    case RAND:
+        c_srand(MACHINE_TIME_REGISTER);
+        return c_rand();
+    case SYSVIDEO:
+        return video;
+        break;
+    case WRITE_TEXT:
+        printf((char *)param2, (int)param3);
+        fflush(stdout);
+        break;
+
+    case THREAD_INITIALLIZE:
+        if (current_thread_num <= 10)
+        {
+            ThreadPointers[current_thread_num] = ContextInitialize((TStackRef)(ThreadStack[current_thread_num - 1] + 2048), (TContextEntry)param2, (void *)param3);
+            current_thread_num++;
+        }
+        break;
+
+    default:
+        break;
     }
-    return -1;
 }
